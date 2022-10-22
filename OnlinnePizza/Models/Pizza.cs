77002a2e78c6bd0b51b5b9bf702a9366e282612b
esha220 <hamnagill66@gmail.com>
2022-10-22 11:45:24 +0500

    1st Week Work

---
+++ OnlinnePizza/Models/Pizza.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace OnlinnePizza.Models
+{
+    public class Pizza
+    {
+       [ JsonPropertyName("id")]
+        public int Pizza_id { get; set; }
+        [JsonPropertyName("name")]
+        public string Pizza_name { get; set; }
+        [JsonPropertyName("image")]
+        public string Pizza_image { get; set; }
+
+        public override string ToString()
+        {
+           return JsonSerializer.Serialize<Pizza>(this);
+        }
+    }
+}
---
+++ OnlinnePizza/Services/JsonPiizzaFile.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using OnlinnePizza.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace OnlinnePizza.Services
+{
+    //for setting the value of IWebHostEnvironment
+    public class JsonPiizzaFile
+    {
+        public JsonPiizzaFile(IWebHostEnvironment webHostEnv)
+        {
+            WebHostEnv = webHostEnv;
+        }
+        public IWebHostEnvironment WebHostEnv { get; }
+        //Accessing Json file path
+        public string JsonFilePath
+        {
+            get
+            {
+                return Path.Combine(WebHostEnv.WebRootPath, "Data", "JSON");                                 //Generae path after combining mulltiple elements
+            }
+        }
+
+        public IEnumerable<Pizza> getPizzaData()                            //Fetch Data from file 
+        {
+           using(var json_file = File.OpenText(JsonFilePath))
+           {
+             return JsonSerializer.Deserialize<Pizza[]>(json_file.ReadToEnd());
+           }    
+             
+        }
+       
+       
+    }
+}
+
+
