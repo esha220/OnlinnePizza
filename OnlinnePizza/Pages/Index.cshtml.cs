00bc067925bb4d8146cc10f63821f928f2b4bca5
esha220 <hamnagill66@gmail.com>
2022-11-01 10:37:57 +0500

    Displaing daa in view

--- OnlinnePizza/Pages/Index.cshtml.cs
+++ OnlinnePizza/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using OnlinnePizza.Models;
+using OnlinnePizza.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +12,22 @@ namespace OnlinnePizza.Pages
 {
     public class IndexModel : PageModel
     {
+
+        public IEnumerable<Pizza> Pizzas { get; private set; }
+        public JsonPiizzaFile PizzaService;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonPiizzaFile pizzaService
+            )
         {
             _logger = logger;
+            PizzaService = pizzaService;
         }
 
         public void OnGet()
         {
-
+            Pizzas = PizzaService.getPizzaData();
         }
     }
 }
